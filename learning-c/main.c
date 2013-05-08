dfcd275d997c0e53e349ac469616c561fba7ac9a
Jonathan Goldsmith <jhghank@gmail.com>
2013-05-07 19:47:00 -0700

    Initial Commit

---
+++ learning-c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  learning-c
+//
+//  Created by Jonathan Goldsmith on 5/7/13.
+//  Copyright (c) 2013 Jonathan Goldsmith. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
