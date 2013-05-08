8a8794746dd5f7e9d44f71d4e1dd17300834f06d
Jonathan Goldsmith <jhghank@gmail.com>
2013-05-07 20:29:07 -0700

    switched static vars to #define

--- fahrenheit.c
+++ fahrenheit.c
@@ -8,22 +8,21 @@
 
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table 
+/* print Fahrenheit-Celsius table
     for floats 0, 20, ..., 300*/
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 void fahrenheitTable() {
     float fahr, celsius;
-    float lower, upper, step;
     
-    lower = 0;
-    upper = 300;
-    step = 20;
-    
-    fahr = lower;
-    while (fahr <= upper) {
+    fahr = LOWER;
+    while (fahr <= UPPER) {
         celsius = (5.0/9.0) * (fahr - 32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
 
